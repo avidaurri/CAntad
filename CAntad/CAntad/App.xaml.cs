0108b8e9df172335cfd9bc4aea8cb15681fa5dfa
avidaurri <40777508+avidaurri@users.noreply.github.com>
2019-11-05 15:39:37 -0600

    mvvm anim y toobar

---
+++ CAntad/CAntad/App.xaml.cs
@@ -0,0 +1,57 @@
+﻿using CAntad.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CAntad
+{
+    public partial class App : Application
+    {
+        public static NavigationPage Navigator { get; set; }
+        public App()
+        {
+            InitializeComponent();
+
+            /* var mainViewModel = MainViewModel.GetInstance();
+
+             if (Settings.IsRemembered)
+             {
+
+                 if (!string.IsNullOrEmpty(Settings.UserSession))
+                 {
+                     mainViewModel.UserSession = JsonConvert.DeserializeObject<UserSession>(Settings.UserSession);
+                 }
+
+                 //mainViewModel.Usuarios = new UsuariosViewModel();
+                 //mainViewModel.Intramuro = new IntramuroViewModel();
+                 //mainViewModel.Promotor = new PromotorViewModel();
+                 mainViewModel.Bienvenido = new BienvenidoViewModel();
+
+                 this.MainPage = new Master(new Bienvenido());
+                 //
+             }
+             else
+             {
+                 mainViewModel.Login = new LoginViewModel();
+                 this.MainPage = new NavigationPage(new LoginPage());
+             }*/
+            //
+            MainPage = new SplashPageIndex();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ CAntad/CAntad/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,17 @@
+﻿using CAntad.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CAntad.Infrastructure
+{
+    public class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
